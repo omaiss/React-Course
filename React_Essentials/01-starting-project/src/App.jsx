d674f6f8390533b493acc81d8bffbbd9cebd7ab9
omaiss <omaisafzal225@gmail.com>
2025-04-21 11:11:23 +0500

    Common best Practice

--- React_Essentials/01-starting-project/src/App.jsx
+++ React_Essentials/01-starting-project/src/App.jsx
@@ -1,37 +1,6 @@
-import reactlogo from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-export function Header() {
-  const desription = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactlogo} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {desription} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// Object destructioring using this syntax
-function CoreConcepts({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { Header } from "./components/Header";
+import CoreConcepts from "./components/CoreConcepts";
 
 function App() {
   return (
---
+++ React_Essentials/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import reactlogo from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export function Header() {
+    const desription = reactDescriptions[genRandomInt(2)];
+  
+    return (
+      <header>
+        <img src={reactlogo} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {desription} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
+  
