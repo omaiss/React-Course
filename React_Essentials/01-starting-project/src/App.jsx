955972bb2659fa2707383609e735ef375959b391
omaiss <omaisafzal225@gmail.com>
2025-04-22 10:53:16 +0500

    Use state for dynamic content updates

--- React_Essentials/01-starting-project/src/App.jsx
+++ React_Essentials/01-starting-project/src/App.jsx
@@ -1,11 +1,19 @@
 import { CORE_CONCEPTS } from "./data";
+import { useState } from "react";   //React Hooks start with use
 import { Header } from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
 import TabButton from "./components/TabButton";
 
 export default function App() {
-  function click() {
-    console.log('click');
+  //React Hooks are called inside React componenets or custom Hooks
+  const [selected_topic, setSelectedTopic] = useState('Please Click a button');
+ 
+  // contains exactly two elements 
+  // let tabContent = 'Please Click a button';
+  function click_fun(selectedbutton) {
+    // selectedbutton = 'components', 'jsx', 'props', 'state'
+    // tabContent = selectedbutton;
+    setSelectedTopic(selectedbutton);
   }
 
   return (
@@ -31,12 +39,14 @@ export default function App() {
           </h2>
           <menu>
             {/* can also use label for the button */}
-            <TabButton click={click}>  Components </TabButton>
-            <TabButton click={click}>  JSX </TabButton>
-            <TabButton click={click}>  Props </TabButton>
-            <TabButton click={click}>  State </TabButton>
+            <TabButton click={() => click_fun('components')}>  Components </TabButton>
+            <TabButton click={() => click_fun('jsx')}>  JSX </TabButton>
+            <TabButton click={() => click_fun('props')}>  Props </TabButton>
+            <TabButton click={() => click_fun('state')}>  State </TabButton>
           </menu>
-          Dynamic Content
+          {/* can't use regular variable for dynamic content, you need to tell React to reevaluate the code 
+          urself, it doesn't auto check {tabContent}*/}
+          {selected_topic}
         </section>
       </main>
     </div>
